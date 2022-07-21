6bf914f3fe21e7c29cd0715000f818016f03c9cf
AliAhmedSamoo <aliahmed.samoo.1@gmail.com>
2022-07-21 01:07:31 -0700

    manu

--- frontend/src/pages/Manu.js
+++ frontend/src/pages/Manu.js
@@ -7,7 +7,7 @@ function Manu() {
             <Navbar />
 
             <div style={({ display: "flex", width: '100%', backgroundColor: '#999', marginTop: '30px', height: '200px' })}>
-                Salads   rdfv
+               
             </div>
 
 
@@ -92,8 +92,117 @@ function Manu() {
 
 
                 </div>
-                <div>
-                    edfcdesfc
+
+
+
+
+                <div style={({ display: 'flex', flexDirection: 'column', width: "30%", gap: '30px' })}>
+
+
+
+
+                    <div style={({ display: 'flex', height: 'auto', boxShadow: '0 0 0 1px', flexDirection: 'column', width: "100%" })}>
+                        <h1> Your Order</h1>
+
+                        <hr style={({ width: '100%' })}></hr>
+                        <div style={({ display: 'flex' })}>
+                            <div style={({ width: '33.33%',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',gap:'0px', height: "auto" })}>
+                                <h3>Order type</h3>
+                                <h2 style={({marginTop:'-20px'})}>Delivery</h2>
+                                <h3>Change</h3>
+                            </div>
+                            <div style={({ width: '33.33%',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',gap:'0px', height: "auto" })}>
+                                <h3>Deliver to</h3>
+                                <h2 style={({marginTop:'-20px'})}>karachi</h2>
+                                <h3>Change</h3>
+                            </div>
+                            <div style={({ width: '33.33%',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',gap:'0px', height: "auto" })}>
+                                <h3>Delivery time</h3>
+                                <h2 style={({marginTop:'-20px'})}>Today, now</h2>
+                                <h3>Change</h3>
+                            </div>
+                        </div>
+
+
+                        <div style={({ display: 'flex' })}>
+                            <div style={({ width: '70%', height: "auto" })}>
+                                <h2>2 xCombo 1</h2>
+                            </div>
+                            <div style={({ display: 'flex', justifyContent: 'right', width: '30%', height: "auto" })}>
+                                <h2>Rs 5000/-</h2>
+                            </div>
+                        </div>
+                        <h5>Deals Items</h5>
+
+
+                        <div style={({ marginLeft: '20px', display: 'flex' })}>
+
+                            <div style={({ width: '70%', height: "auto" })}>
+                                <h5>2 xCombo 1</h5>
+                            </div>
+                            <div style={({ display: 'flex', justifyContent: 'right', width: '30%', height: "auto" })}>
+                                <h5>free</h5>
+                            </div>
+
+                        </div>
+
+                        <div style={({ marginLeft: '20px', display: 'flex' })}>
+
+                            <div style={({ width: '70%', height: "auto" })}>
+                                <h5>2 xCombo 1</h5>
+                            </div>
+                            <div style={({ display: 'flex', justifyContent: 'right', width: '30%', height: "auto" })}>
+                                <h5>free</h5>
+                            </div>
+
+                        </div>
+
+                        <div style={({ marginLeft: '20px', display: 'flex' })}>
+
+                            <div style={({ width: '70%', height: "auto" })}>
+                                <h5>2 xCombo 1</h5>
+                            </div>
+                            <div style={({ display: 'flex', justifyContent: 'right', width: '30%', height: "auto" })}>
+                                <h5>free</h5>
+                            </div>
+
+                        </div>
+
+                        <div style={({ marginLeft: '20px', display: 'flex' })}>
+
+                            <div style={({ width: '70%', height: "auto" })}>
+                                <h5>2 xCombo 1</h5>
+                            </div>
+                            <div style={({ display: 'flex', justifyContent: 'right', width: '30%', height: "auto" })}>
+                                <h5>free</h5>
+                            </div>
+
+                        </div>
+                        <div style={({ marginLeft: '20px', display: 'flex' })}>
+
+                            <div style={({ width: '70%', height: "auto" })}>
+                                <h5>Edit Extras</h5>
+                            </div>
+
+
+                        </div>
+                        <div style={({ display: 'flex', gap: '10px', marginLeft: '5px' })}>  <a> <h5>Repeat</h5> </a><h5>Remove</h5></div>
+
+
+                        <div style={({ display: 'flex', width: '100%', height: 'auto', backgroundColor: '#999' })}>
+                            <div>  <h3>Item cost  Rs:  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    2,198/-</h3>
+                                <h3>Delivery Fee Rs:    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      198/-</h3>
+                                ------------------------
+                                <h1>Order Total Rs: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 5000/-</h1>
+                                <h5>All prices are inclusive of 13% GST</h5>
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <div style={({ justifyTracks: 'center', alignItems: 'center', display: 'flex', height: '50px', backgroundColor: "#999", boxShadow: '0 0 0 1px', flexDirection: 'column', width: "100%" })}>
+                        <h1>Place Order</h1>
+                    </div>
                 </div>
 
 
