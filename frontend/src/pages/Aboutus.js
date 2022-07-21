9e91d32b39459493809beb232f6171705e5be797
sajidarafique <sajidatabssum1701173@gmail.com>
2022-07-22 01:07:37 +0500

    edit some of aboutUs page content

--- frontend/src/pages/Aboutus.js
+++ frontend/src/pages/Aboutus.js
@@ -11,16 +11,20 @@ function Aboutus() {
       <div className='submain1 box'>
         <div className='main_content'>
           <h1>ClockiFood</h1>
+        </div>
+        <div>
           <p>About us Formed in the food-loving business hub of Karachi, foodpanda offers city-dwellers the most convenient way to order high quality meals. Our goal is simple: To become the world’s fastest, most innovative business in the good food sector</p>
         </div>
       </div>
       <div className='submain2'>
         <div className='main2_left'>
-          <h2>write here with this color</h2>
+          {/*<h2>write here with this color</h2>*/}
+          <h1 className='h2'>HOW IT WORKS</h1>
+          <h2 className='h2'>     <b>ORDER         SCHEDULE     ENJOY      RELAX </b>    </h2>
         </div>
-        <div className='main2_right'>
+       {/* <div className='main2_right'>
         <p>write here with this color</p>
-        </div>
+  </div>}*/}
       </div>
       <div className='submain3'>
         <div className='clients'>
@@ -38,15 +42,22 @@ function Aboutus() {
                 </p>
               </div>
             </div>
-
-
           </Carousel> */}
+          
         </div>
       </div>
+     
       <div className='submain4'>
-      {/* <MasonryImageList /> */}
-      </div>
+         {/* <MasonryImageList /> */}
+         <h3>OUR MISSION</h3><br />
+           <p></p><p>"Bringing good food into your everyday. That's our mission.</p><br />
+           <p>That means we don't just deliver--we bring it, always going the extra mile to make your experience memorable.</p><br />
+           <p>And it means this is delicious food you can enjoy everyday: from vibrant salads for healthy office lunches, to indulgent family-sized pizzas, to fresh sushi for a romantic night in. Whatever you crave, we can help."</p>
+           <p></p>
+        
+        </div>
       <div className='submain5'></div>
+      
     </div>
   )
 
